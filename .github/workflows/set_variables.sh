a8a440f0abf5b3f280836e34a9e90fa877eb42c0
metworkbot <metworkbot@metwork-framework.org>
2021-02-03 16:50:22 +0000

    build: sync common files from github_organization_management repository

---
+++ .github/workflows/set_variables.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+
+
+
+
+set -x
+
+if test "centos8" = ""; then
+    echo "ERROR: OS env is empty"
+    exit 1
+fi
+
+case "${GITHUB_EVENT_NAME}" in
+    repository_dispatch)
+        BRANCH=${PAYLOAD_BRANCH};;
+    push)
+        case "${GITHUB_REF}" in
+            refs/heads/*)
+                BRANCH=${GITHUB_REF#refs/heads/};;
+            *)
+                BRANCH=null;
+        esac;;
+esac
+if [ -z ${BRANCH} ]; then
+  BRANCH=null
+fi
+TAG_BRANCH="metwork/mfbaseplugins-centos8-buildimage2:${BRANCH}"
+TAG_LATEST=""
+if test "${BRANCH}" = "master"; then
+    TAG_LATEST="metwork/mfbaseplugins-centos8-buildimage2:latest"
+fi 
+echo "::set-output name=branch::${BRANCH}"
+echo "::set-output name=tag_branch::${TAG_BRANCH}"
+echo "::set-output name=tag_latest::${TAG_LATEST}"
